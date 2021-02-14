21ea50286a9b3a77c6c43c18c6106fb2600c937a
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2021-02-14 23:27:40 +0530

    navigation properly without change the top bar

--- app/src/main/java/com/insharp/android/drawer_with_mapbox/MainActivity.java
+++ app/src/main/java/com/insharp/android/drawer_with_mapbox/MainActivity.java
@@ -1,8 +1,13 @@
 package com.insharp.android.drawer_with_mapbox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new FirstFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            /* By using switch we can easily get the selected fragment by using there id. */
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.firstFragment:
+                    selectedFragment = new FirstFragment();
+                    break;
+                case R.id.secondFragment:
+                    selectedFragment = new SecondFragment();
+                    break;
+                case R.id.thirdFragment:
+                    selectedFragment = new ThirdFragment();
+                    break;
+                default:
+                    selectedFragment = null;
+                    break;
+            }
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment, selectedFragment)
+                    .commit();
+            return true;
+        }
+    };
 }
